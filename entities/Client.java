be0b9ce0d3fe5541e600bb63b3b7197701c93b2e
OrBibi <Orbibiariel@gmail.com>
2024-12-05 19:37:29 +0200

    start

--- entities/Client.java
+++ entities/Client.java
@@ -1,8 +1,22 @@
 package entities;
 
+import enums.Gender;
+
 public class Client extends Person {
+    public static int totalID=0;
     private int id;
 
-    public Client()
+    public Client(String name, int balance, Gender gender, String dateOfBirth, int salary){
+        super(name, balance, gender, dateOfBirth);
+        totalID++;
+        this.id = totalID;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
