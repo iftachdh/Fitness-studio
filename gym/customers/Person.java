43bcc9e780158f5e5bd5be082ad113441f3cc859
OrBibi <Orbibiariel@gmail.com>
2024-12-19 12:55:18 +0200

    start

--- gym/customers/Person.java
+++ gym/customers/Person.java
@@ -79,7 +79,9 @@ public class Person {
     }
 
     public int getAge(){
-        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate now = LocalDate.parse("25-12-2024", formatter);
+        //LocalDate now = LocalDate.now();
         Period period = Period.between(dateOfBirth, now);
         return period.getYears();
     }
