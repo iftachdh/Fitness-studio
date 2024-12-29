8daf7ef5062a8d0fc889f34ad2e7fb2645478e0c
iftachdh <iftach6@gmail.com>
2024-12-29 21:29:27 +0200

    iftach

--- gym/management/Sessions/SessionsFactory.java
+++ gym/management/Sessions/SessionsFactory.java
@@ -10,50 +10,35 @@ import gym.management.Session;
  *  to add a new type of sessions to the factory:
  *      1. You need to add a new session type to "SessionType" Enum class
  *      2. You need to create a new class of the new type (Extend session)
- *      3. You need to add in this class "else if"
+ *      3. You need to add a new case
  */
 
 public class SessionsFactory {
     private static SessionsFactory instance;
 
-    private SessionsFactory(){}
+    private SessionsFactory() {
+    }
 
-    public static SessionsFactory getInstance(){
-        if(instance==null){
-            instance=new SessionsFactory();
+    public static SessionsFactory getInstance() {
+        if (instance == null) {
+            instance = new SessionsFactory();
         }
         return instance;
     }
 
     public Session CreateSession(SessionType type, String dateAndHour, ForumType forumType, Instructor instructor) throws InstructorNotQualifiedException {
-
-        if(type.equals(SessionType.Pilates)){
-            if (instructor.get_sessions().contains(SessionType.Pilates)) {
-                return new Pilates(type, dateAndHour, forumType, instructor);
-            }
-            else throw new InstructorNotQualifiedException ("Error: Instructor is not qualified to conduct this session type.");
+        if (!instructor.get_sessions().contains(type)) {
+            throw new InstructorNotQualifiedException("Error: Instructor is not qualified to conduct this session type.");
         }
-
-        else if(type.equals(SessionType.MachinePilates)){
-            if (instructor.get_sessions().contains(SessionType.MachinePilates)) {
-                return new MachinePilates(type, dateAndHour, forumType, instructor);
-            }
-            else throw new InstructorNotQualifiedException ("Error: Instructor is not qualified to conduct this session type.");
-        }
-
-        else if(type.equals(SessionType.ThaiBoxing)){
-            if (instructor.get_sessions().contains(SessionType.ThaiBoxing)) {
-                return new ThaiBoxing(type, dateAndHour, forumType, instructor);
-            }
-            else throw new InstructorNotQualifiedException ("Error: Instructor is not qualified to conduct this session type.");
-        }
-
-        else if(type.equals(SessionType.Ninja)){
-            if (instructor.get_sessions().contains(SessionType.Ninja)) {
-                return new Ninja(type, dateAndHour, forumType, instructor);
-            }
-            else throw new InstructorNotQualifiedException ("Error: Instructor is not qualified to conduct this session type.");
-        }
-        else return null;
+        return switch (type) {
+            case Pilates -> new Pilates(type, dateAndHour, forumType, instructor);
+            case MachinePilates -> new MachinePilates(type, dateAndHour, forumType, instructor);
+            case ThaiBoxing -> new ThaiBoxing(type, dateAndHour, forumType, instructor);
+            case Ninja -> new Ninja(type, dateAndHour, forumType, instructor);
+        };
     }
 }
+
+
+
+
